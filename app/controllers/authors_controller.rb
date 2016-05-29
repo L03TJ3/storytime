856bece20c8557bb7b4567658af0eaf4aecbd611
l03tj3 <laurenceweening@gmail.com>
2016-05-29 21:29:42 +0200

    Add index/show action to author controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,11 @@
 class AuthorsController < ApplicationController
+
+  def index
+    @authors = Author.all.sort_by { |author| author.name }
+  end
+
+  def show
+    @author = Author.find( params[:id] )
+  end
+
 end
