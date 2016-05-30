4e82286bd9e9b6796ea6c3ad8bd1d5be499debab
l03tj3 <laurenceweening@gmail.com>
2016-05-30 14:19:37 +0200

    Add controller actions for stories

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,37 @@
 class StoriesController < ApplicationController
+  def index
+    @stories = Story.all.sort_by { |story| story.title }
+  end
+
+  def show
+    @story = Story.find( params[:id] )
+  end
+
+  def new
+    @story = Story.new
+    @story.author_id = params[:author_id]
+  end
+
+  def create
+    @story = Story.new( story_params )
+    @tags = Story.tagging(@story.content)
+
+    if @story.save
+      @tags.each do |tag_name|
+        if tag = Tag.find_by( name: tag_name )
+          StoryTagging.create( story: @story, tag: tag )
+        end
+      end
+      redirect_to poems_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def story_params
+      params.require( :story ).permit( :title, :content, :description, :date,
+                                                                        :author_id )
+    end
 end
