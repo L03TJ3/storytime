0fea126c182168e85904d953e2c087ef71e86e2e
l03tj3 <laurenceweening@gmail.com>
2016-05-30 14:14:34 +0200

    Add relations for story model

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,3 +1,11 @@
 class Story < ActiveRecord::Base
   belongs_to :author
+  has_many :story_taggings
+  has_many :tags, through: :story_taggings
+
+  def self.tagging(text)
+    text_array = text.scan(/\w+/).map(&:downcase)
+    tags_array = Tag.pluck(:name)
+    tags_array & text_array
+  end
 end
